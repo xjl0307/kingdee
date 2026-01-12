dcc4cb78c54ffd4fe8d67d2ed62770481d819ac6
xjl0307 <35141253+xjl0307@users.noreply.github.com>
2026-01-12 10:52:50 +0800

    Delete randomInt.ts

--- randomInt.ts
+++
@@ -1,7 +0,0 @@
-const randomInt = function () {
-    let max = 9999999999999999
-    let min = 1000000000000000
-    return Math.floor(Math.random() * (max - min + 1)) + min
-}
-
-export default randomInt
